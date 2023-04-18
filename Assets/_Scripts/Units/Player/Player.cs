889dcf2305a6bcc31641c953dc04307927f0028d
MoosieySroosiey <kl1timch@gmail.com>
2023-04-18 22:10:36 +0200

    Began InverseKinematics implementation

--- Assets/_Scripts/Units/Player/Player.cs
+++ Assets/_Scripts/Units/Player/Player.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class Player : Character
 {
-  
+
+    private Rigidbody2D _rigidbody;
+  void Awake() 
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    private void OnAnimatorMove()
+    {
+        
+    }
 }
