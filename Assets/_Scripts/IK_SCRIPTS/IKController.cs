f79ac138bfede545018257a93a1d996638344e03
MoosieySroosiey <kl1timch@gmail.com>
2023-04-24 23:14:14 +0200

    Some changes

--- Assets/_Scripts/IK_SCRIPTS/IKController.cs
+++ Assets/_Scripts/IK_SCRIPTS/IKController.cs
@@ -35,7 +35,7 @@ public class IKController : MonoBehaviour
         float xDiff = x2 - x1;
         float yDiff = y2 - y1;
         return (float)((float)Mathf.Rad2Deg * Math.Atan2(yDiff, xDiff));
-        
+
     }
 
     float pointDistance(float x1, float y1, float x2, float y2)
@@ -54,25 +54,25 @@ public class IKController : MonoBehaviour
         return (float)(len * Math.Sin(dir));
     }
 
-    void createIK(float lengthCalf, float lengthThigh)
-    {
+    //void createIK(float lengthCalf, float lengthThigh) //will be used later for proper initialization of variables needed for IK calculations
+    //{
 
 
-        motion_counter = 0; //counter for animating the leg movement
+    //    motion_counter = 0; //counter for animating the leg movement
 
-        thigh = lengthCalf; //thigh length
+    //    thigh = lengthCalf; //thigh length
 
-        calf = lengthThigh; //calf length
+    //    calf = lengthThigh; //calf length
 
-        facingdirection = 1.3f; //direction for knee (but also character in general)
+    //    facingdirection = 1.3f; //direction for knee (but also character in general)
 
-        legspeed = 1; //speed of movement
+    //    legspeed = 1; //speed of movement
 
-        movingdirection = 1.3f; //direction of movement
+    //    movingdirection = 1.3f; //direction of movement
 
-        gait = 0.1f; //size of step
+    //    gait = 0.1f; //size of step
 
-    }
+    //}
 
     void drawLeg(float lengthCalf, float lengthThigh, float hipX, float hipY, float off)
     {
@@ -108,17 +108,14 @@ public class IKController : MonoBehaviour
 
 
         //Sin-> Horizontal movement, Cos-> Veritcal movement of the "foot"
-
-
-        ax = x + lengthDirX(Convert.ToSingle((gait) * ((aLength + bLength) / 4) * (Mathf.Rad2Deg * Math.Sin(motionCounter)) - ((legspeed * 1.25)) * 2), movingDirection);
+        ax = x + lengthDirX(Convert.ToSingle((gait) * ((aLength + bLength) / 4) * (Mathf.Rad2Deg * -Math.Sin(motionCounter)) - ((legspeed * 1.25)) * 2), movingDirection);
         //x=location x
 
-        ay = (float)(y + ((gait) * ((aLength + bLength) / 6) * (-Math.Cos((motionCounter)) - 1f)));
+        ay = (float)(y + ((gait) * ((aLength + bLength) / 6) * (Mathf.Rad2Deg * Math.Cos((motionCounter)) - 1f)));
         //y=location y
 
         ///IK CALCULATION///
-        alpha = pointDirection(Ax, Ay, ax, ay); //angle between hip and foot
-
+        alpha = pointDirection(Ax, Ay, ax, ay); //angle between hip and foo
         cLength = Math.Min(pointDistance(Ax, Ay, ax, ay), (aLength + bLength)); //distance between hip and foot, restricted to total limb length
 
         Bx = Ax + lengthDirX(cLength, alpha); //foot x position
@@ -138,27 +135,27 @@ public class IKController : MonoBehaviour
         C2y = iy + lengthDirY(pointDistance(ix, iy, Cx, Cy) * kneeMod, pointDirection(ix, iy, Cx, Cy));//"3D" knee y position
 
 
-        ///////////////////////////////////////////////////////////////////////
-        //////////////////DRAWING THE SPRITES (might move to ext)///////////////
-        ///////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////
+        //////////////////DRAWING THE SPRITES (might move to ext)////////////
+        /////////////////////////////////////////////////////////////////////
 
         ///draw_line_width(Ax+lengthdir_x(argument5,facingdirection+90),Ay,C2x+lengthdir_x(argument5,facingdirection+90),C2y,3)
         ///draw_line_width(C2x+lengthdir_x(argument5,facingdirection+90),C2y,Bx+lengthdir_x(argument5,facingdirection+90),By,
-        Debug.DrawLine(new Vector2(Ax + lengthDirX(off, facingDirection + 90), Ay), new Vector2(C2x + lengthDirX(off, facingDirection + 90), C2y), Color.magenta);
-        Debug.DrawLine(new Vector2(C2x + lengthDirX(off, facingDirection + 90), C2y), new Vector2(Bx + lengthDirX(off, facingDirection + 90), By), Color.blue);
+        Debug.DrawLine(new Vector2(Ax + lengthDirX(off, facingDirection ), Ay), new Vector2(C2x + lengthDirX(off, facingDirection ), C2y), Color.magenta);
+        Debug.DrawLine(new Vector2(C2x + lengthDirX(off, facingDirection ), C2y), new Vector2(Bx + lengthDirX(off, facingDirection ), By), Color.blue);
 
 
 
 
 
         Debug.Log("Ax:" + Ax + " Ay:" + Ay + " ix" + ix + " iy:" + iy + " Bx" + Bx + " By" + By + "Cx:" + Cx + " Cy:" + Cy + " C2x" + C2x + " C2y:" + C2y);
-    //    sr.sprite = mySprite;//drawing sprite not used now
+        //    sr.sprite = mySprite;//drawing sprite not used now
     }
 
     void motionCounterStep()
     {
         motionCounter += (float)(Math.Pow(legspeed, 0.4));//this is the counting variable for the animation
-        motionCounter = motionCounter % 360;  //limit the variable between 0 and 360
+        motionCounter = motionCounter % 180;  //limit the variable between 0 and 360
     }
     void Start()
     {
@@ -167,31 +164,33 @@ public class IKController : MonoBehaviour
         // createIK(0.10f, 0.8f);//Crete the IK on Start
 
         sr = gameObject.GetComponent<SpriteRenderer>();
-        sr.color = new Color(0.9f, 0.9f, 0.9f, 1.0f);
+        sr.color = new Color(0.1f, 0.9f, 0.9f, 1.0f);
         rb = player.GetComponent<Rigidbody2D>();
-        if (legspeed > 0.1)
-        {
-            motionCounterStep();
-        }
+
 
     }
 
     private void FixedUpdate()
     {
-        x = player.transform.position.x;////NIE WIEM CZY TO MA SENS
-        y = player.transform.position.y;
+        x = player.transform.localPosition.x;
+        y = player.transform.localPosition.y;
+        Debug.Log("x: " + x + "/ y: " + y);
+
 
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
         facingDirection = pointDirection(x, y, mousePos.x, mousePos.y);
         movingDirection = pointDirection(x, y, mousePos.x, mousePos.y);
         y -= .08f;
-        Debug.Log("x: " + x + "/ y: " + y);
 
 
 
-        drawLeg(0.1f, 0.12f, x, y, 0.05f); drawLeg(0.1f, 0.12f, x, y, -0.05f);
 
+        drawLeg(0.06f, 0.08f, x, y, 0.05f); drawLeg(0.06f, 0.08f, x, y, -0.05f);
 
+        if (legspeed > 0.001f)
+        {
+            motionCounterStep();
+        }
 
         //  transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
