c54ebaf2b8f0ced279d1a02b45801c66337ccbf7
MoosieySroosiey <kl1timch@gmail.com>
2023-05-24 01:38:50 +0200

    BaseCharacter class upgrade

--- Assets/_Scripts/Units/Character.cs
+++ Assets/_Scripts/Units/Character.cs
@@ -1,3 +1,4 @@
+using Assets._Scripts.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,4 +9,11 @@ public abstract class Character : MonoBehaviour
     public int hitPointsMax;
     public int speed;
 
+    public abstract void SetStats(Stats stats);
+
+  //  public abstract void TakeDamage(float dmg, List<ConditionBase> conditions);
+
+    public abstract bool TryMove(Vector2 direction);
+
+    public abstract void Die();
 }
--- Assets/_Scripts/Utilities/Stats.cs
+++ Assets/_Scripts/Utilities/Stats.cs
@@ -1,27 +1,35 @@
 ﻿using System;
-using System.ComponentModel;
+using UnityEngine;
 
 namespace Assets._Scripts.Utilities
 {
+    /// <summary>
+    /// Keeping base stats as a struct on the scriptable keeps it flexible and easily editable.
+    /// </summary>
     [Serializable]
     public struct Stats
     {
-        [DefaultValue(5)]
-        public int MaxHp { get; set; }
+        [SerializeField]
+        int _MaxHp;
+        public int MaxHp { get { return _MaxHp; } set { _MaxHp = value; } } 
 
-        [DefaultValue(5)]
-        public int CurrentHp { get; set; }
+        int _CurrentHp;
+        public int CurrentHp { get { return _CurrentHp; } set { _CurrentHp = value; } }
 
-        [DefaultValue(1.0f)]
-        public float Armor { get; set; } //if u getting damaged for some (dmg) u get (dmg) * (armor)
+        [SerializeField]
+        float _Armor;
+        public float Armor { get { return _Armor; } set { _Armor = value; } } //if u getting damaged for some (dmg) u get (dmg) * (armor)
 
-        [DefaultValue(1.0f)]
-        public float MovementSpeed { get; set; }
+        [SerializeField]
+        float _MovementSpeed;
+        public float MovementSpeed { get { return _MovementSpeed; } set { _MovementSpeed = value; } }
 
-        [DefaultValue(1.0f)]
-        public float DmgModifier { get; set; }
+        [SerializeField]
+        float _DmgModifier;
+        public float DmgModifier { get { return _DmgModifier; } set { _DmgModifier = value; } }
 
-        [DefaultValue(1.0f)]
-        public float CooldownModifier { get; set; }
+        [SerializeField]
+        float _CooldownModifier;
+        public float CooldownModifier { get { return _CooldownModifier; } set { _CooldownModifier = value; } }
     }
 }
