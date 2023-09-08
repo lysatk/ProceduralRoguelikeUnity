37831e830da5414f71abd1a2392208c309c7b279
MoosieySroosiey <kl1timch@gmail.com>
2023-09-08 03:40:53 +0200

    PauseMenu Began + PauseChecks in InputMethods

--- Assets/_Scripts/Units/Heroes/HeroUnitBase.cs
+++ Assets/_Scripts/Units/Heroes/HeroUnitBase.cs
@@ -442,7 +442,7 @@ public class HeroUnitBase : UnitBase
 
     void OnPrimaryAttack()
     {
-        if (!_isDead)
+        if (!_isDead && !GameManager.gamePaused)
             if (Time.time > primaryCooldownCounter)
             {
                 CastSpell(PrimarySpell);
@@ -452,7 +452,7 @@ public class HeroUnitBase : UnitBase
 
     void OnSecondaryAttack()
     {
-        if (!_isDead)
+        if (!_isDead && !GameManager.gamePaused)
             if (Time.time > secondaryCooldownCounter)
             {
                 CastSpell(SecondarySpell);
@@ -462,7 +462,7 @@ public class HeroUnitBase : UnitBase
 
     void OnQSpell()
     {
-        if (!_isDead)
+        if (!_isDead && !GameManager.gamePaused)
             if (Time.time > QCooldownCounter)
             {
                 CastSpell(QSpell);
@@ -472,7 +472,7 @@ public class HeroUnitBase : UnitBase
 
     void OnESpell()
     {
-        if (!_isDead)
+        if (!_isDead && !GameManager.gamePaused)
             if (Time.time > ECooldownCounter)
             {
                 CastSpell(ESpell);
@@ -482,7 +482,7 @@ public class HeroUnitBase : UnitBase
 
     void OnDodge()
     {
-        if (!_isDead)
+        if (!_isDead && !GameManager.gamePaused)
             if (Time.time > DashCooldownCounter)
             {
                 CastSpell(DashSpell);
