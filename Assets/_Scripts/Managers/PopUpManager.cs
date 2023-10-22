b26143e448a39eff94ae5c8ba05e110bbb081a5c
MoosieySroosiey <kl1timch@gmail.com>
2023-10-22 04:40:08 +0200

    UI update + tweaks

--- Assets/_Scripts/Managers/PopUpManager.cs
+++ Assets/_Scripts/Managers/PopUpManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using Unity.VisualScripting;
 
 public class PopUpManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+  public List<PopUpModel>  PopUpModels = new List<PopUpModel>();
+
+
+    public void ShowPopUp(int popUpId)//, float xPos,float yPos)
     {
-        
+        PopUpModel popUpModel =PopUpModels.FirstOrDefault(popUp=>popUp.popUpId == popUpId);
+        if (popUpModel==null)
+        {
+            Debug.LogWarning("Tried using a popUpId without a respective popUp");
+            return; 
+        }
+        else
+        {
+            Instantiate(popUpModel.popUpObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+}
+
+public class PopUpModel
+{
+    public int popUpId;
+    public GameObject popUpObject;
+
 }
