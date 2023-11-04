a3fa676196488d05ff77f8aecac782cfe64a7652
MoosieySroosiey <kl1timch@gmail.com>
2023-11-04 06:05:09 +0100

    UI code almost done

--- Assets/_Scripts/UiScripts/CharacterStatsUI.cs
+++ Assets/_Scripts/UiScripts/CharacterStatsUI.cs
@@ -1,72 +1,147 @@
 using UnityEngine;
+using TMPro; // Add this namespace to use TextMeshPro
 using UnityEngine.UI;
 
 public class CharacterStatsUI : MonoBehaviour
 {
-    public HeroUnitBase player; // Reference to the player's stats script
-    public Text pointsText; // Display for available points
-    public Text hpText, armorText, speedText, dmgModText, cooldownText; // Displays for each stat
-    public Button fullHealButton; // Button for full heal
+    public HeroUnitBase player;
+    public TextMeshProUGUI pointsText;
+    public TextMeshProUGUI hpText, armorText, speedText, dmgModText, cooldownText;
+    public Button fullHealButton;
 
-    private int availablePoints = 5; // Total points available for the player to use
+    private int availablePoints = 5;
+    private const int maxPoints = 5; // Maximum points that the player can have
 
-    private void Start()
+    private void OnEnable()
     {
-        player=FindObjectOfType<HeroUnitBase>();
-        UpdateUI();
-        // Disable the full heal button if the player is at max health
-        fullHealButton.interactable = player.stats.CurrentHp < player.stats.MaxHp;
+        if (GameManager.Player != null)
+        {
+            player = GameManager.Player.GetComponent<HeroUnitBase>();
+            if (player != null)
+            {
+                UpdateUI();
+                fullHealButton.interactable = player.stats.CurrentHp < player.stats.MaxHp;
+            }
+            else
+            {
+                Debug.LogError("HeroUnitBase component not found on the player game object!");
+            }
+        }
+        else
+        {
+            Debug.LogError("Player game object reference not set!");
+        }
     }
 
-    public void ModifyStat(string stat, int amount)
+
+
+    // Increase methods
+    public void IncreaseHP()
     {
-        if (availablePoints <= 0 && amount > 0) return; // No points to use for upgrading
+        if (availablePoints <= 0) return;
+        player.stats.MaxHp++;
+        availablePoints--;
+        UpdateUI();
+    }
 
-        switch (stat)
-        {
-            case "HP":
-                player.stats.MaxHp += amount;
-                break;
-            case "Armor":
-                player.stats.Armor += amount;
-                break;
-            case "Speed":
-                player.stats.MovementSpeed += amount;
-                break;
-            case "DmgMod":
-                player.stats.DmgModifier += amount;
-                break;
-            case "Cooldown":
-                player.stats.CooldownModifier += amount;
-                break;
-        }
+    public void IncreaseArmor()
+    {
+        if (availablePoints <= 0) return;
+        player.stats.Armor++;
+        availablePoints--;
+        UpdateUI();
+    }
 
-        availablePoints -= amount;
+    public void IncreaseSpeed()
+    {
+        if (availablePoints <= 0) return;
+        player.stats.MovementSpeed++;
+        availablePoints--;
+        UpdateUI();
+    }
+
+    public void IncreaseDmgMod()
+    {
+        if (availablePoints <= 0) return;
+        player.stats.DmgModifier++;
+        availablePoints--;
+        UpdateUI();
+    }
+
+    public void IncreaseCooldown()
+    {
+        if (availablePoints <= 0) return;
+        player.stats.CooldownModifier++;
+        availablePoints--;
+        UpdateUI();
+    }
+
+    public void DecreaseHP()
+    {
+        if (availablePoints == maxPoints || player.stats.MaxHp <= 1) return;
+        player.stats.MaxHp--;
+        availablePoints++;
+        UpdateUI();
+    }
+
+    public void DecreaseArmor()
+    {
+        if (availablePoints == maxPoints || player.stats.Armor <= 1) return;
+        player.stats.Armor--;
+        availablePoints++;
+        UpdateUI();
+    }
+
+    public void DecreaseSpeed()
+    {
+        if (availablePoints == maxPoints || player.stats.MovementSpeed <= 1) return;
+        player.stats.MovementSpeed--;
+        availablePoints++;
+        UpdateUI();
+    }
+
+    public void DecreaseDmgMod()
+    {
+        if (availablePoints == maxPoints || player.stats.DmgModifier <= 1) return;
+        player.stats.DmgModifier--;
+        availablePoints++;
+        UpdateUI();
+    }
+
+    public void DecreaseCooldown()
+    {
+        if (availablePoints == maxPoints || player.stats.CooldownModifier <= 1) return;
+        player.stats.CooldownModifier--;
+        availablePoints++;
         UpdateUI();
     }
 
     public void FullHeal()
     {
-        if (availablePoints <= 0) return; // No points to use for healing
+        if (availablePoints <= 0) return;
 
         player.stats.CurrentHp = player.stats.MaxHp;
-        availablePoints -= 1; // Full heal costs 1 point
+        availablePoints--; // Full heal costs 1 point
         UpdateUI();
     }
 
     private void UpdateUI()
     {
-        // Update the text fields with the current stats
         hpText.text = "HP: " + player.stats.MaxHp;
         armorText.text = "Armor: " + player.stats.Armor;
         speedText.text = "Speed: " + player.stats.MovementSpeed;
         dmgModText.text = "Damage Mod: " + player.stats.DmgModifier;
         cooldownText.text = "Cooldown: " + player.stats.CooldownModifier;
-
-        // Update the points display
         pointsText.text = "Points: " + availablePoints;
-
-        // Disable the full heal button if the player is at max health or no points left
         fullHealButton.interactable = player.stats.CurrentHp < player.stats.MaxHp && availablePoints > 0;
+    }
+
+    public void HideUI()
+    {
+        gameObject.SetActive(false);
+    }
+    public void ShowUI()
+    {
+        gameObject.SetActive(true);
     }
 }
